2266c6f9968ebc295713c68c144992d12aa0085e
Vaishnav Nair <64798176+iamvaishnav@users.noreply.github.com>
2020-09-23 15:06:10 +0530

    add filter function

--- app.js
+++ app.js
@@ -13,5 +13,61 @@ function changeHeader() {
 
 
 }
-
 window.addEventListener('resize', changeHeader);
+
+//filter functionality
+const filterContainer = document.querySelector('.filter-container')
+const tags = document.querySelectorAll('.tags');
+const clear = document.querySelector('.clear');
+const filterArea = document.querySelector('.filters');
+let closeBtn;
+let itemName;
+let passArray = [];
+tags.forEach(current => {
+    current.addEventListener('click', () => {
+
+        if (!(passArray.includes(current.textContent))) {
+            applyFilters(current);
+            passArray.push(current.textContent)
+        }
+
+    })
+})
+
+function applyFilters(tag) {
+
+    filterContainer.style.display = 'flex';
+    let el = '<div class="filter-item"> <span class="item-name">%HTML%</span><span class="close"><img src="img/icon-remove.svg" alt="remove"></span></div>';
+    let newEl = el.replace('%HTML%', tag.textContent)
+    filterArea.insertAdjacentHTML('beforeend', newEl);
+    closeBtn = document.querySelectorAll('.close');
+    console.log(closeBtn)
+}
+
+// clear button
+clear.addEventListener('click', () => {
+    filterContainer.style.display = 'none';
+    for (let i = 0; i < filterArea.childNodes.length;) {
+        filterArea.childNodes[i].remove();
+
+    }
+    passArray.splice(0, passArray.length);
+})
+
+//function to clear the tag
+/*
+function closeTag() {
+    closeBtn.forEach(current => {
+
+        current.addEventListener('click', (event) => {
+            console.log(current);
+            console.log(event);
+        })
+        console.error('not invoked yet')
+    })
+
+
+}
+
+closeTag();
+*/
