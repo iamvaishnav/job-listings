6d17e696a554935c58d4598189f6ff8c2494b0f6
Vaishnav Nair <64798176+iamvaishnav@users.noreply.github.com>
2020-10-01 16:10:36 +0530

    Bug fixes

--- app.js
+++ app.js
@@ -16,10 +16,13 @@ function changeHeader() {
 window.addEventListener('resize', changeHeader);
 
 //filter functionality
+
 const filterContainer = document.querySelector('.filter-container')
 const tags = document.querySelectorAll('.tags');
 const clear = document.querySelector('.clear');
 const filterArea = document.querySelector('.filters');
+const items = document.querySelectorAll('.item');
+
 let closeBtn;
 let itemName;
 let passArray = [];
@@ -31,12 +34,15 @@ tags.forEach(current => {
             applyFilters(current);
             passArray.push(current.textContent);
         }
+
     })
+
 })
 
 
 
 function applyFilters(tag) {
+
 
     filterContainer.style.display = 'flex';
     let el = '<div class="filter-item"> <span class="item-name">%HTML%</span><span class="close"><img src="img/icon-remove.svg" alt="remove"></span></div>';
@@ -44,6 +50,7 @@ function applyFilters(tag) {
     filterArea.insertAdjacentHTML('beforeend', newEl);
     closeBtn = document.querySelectorAll('.close');
     closeTag();
+    itemsFilter(tag.textContent);
 
 }
 
@@ -60,6 +67,8 @@ function clearFilters() {
 
     }
     passArray.splice(0, passArray.length);
+
+    itemsShow();
 }
 
 //function to clear the tag
@@ -71,21 +80,18 @@ function closeTag() {
         current.addEventListener('click', (event) => {
 
             if (event.target.classList.contains('close')) {
+
                 updatePassArray(event.target.parentNode.firstElementChild.textContent);
-
-
-                console.log('hello');
                 event.target.parentNode.remove();
 
             } else {
+
                 updatePassArray(event.target.parentNode.parentNode.firstElementChild.textContent);
-
-
-                console.log('hello');
                 event.target.parentNode.parentNode.remove();
 
             }
             if (filterArea.childElementCount === 0) {
+
                 clearFilters();
             }
 
@@ -93,6 +99,7 @@ function closeTag() {
 
     })
 }
+// function to update pass array
 
 function updatePassArray(value) {
     passArray.forEach((current, index, arr) => {
@@ -101,4 +108,51 @@ function updatePassArray(value) {
         }
     })
 
+}
+
+// function to hide items
+
+function itemsHide() {
+    items.forEach(current => current.style.display = 'none')
+}
+
+// function to show all items
+
+function itemsShow() {
+    items.forEach(current => current.style.display = 'grid')
+}
+
+// toggle display of items boxes
+
+function itemsFilter(tag) {
+
+
+    items.forEach(current => {
+
+        let languages = current.dataset.languages.split(',');
+        let tools = current.dataset.tools.split(',')
+
+        if (current.dataset.role === tag || current.dataset.level === tag) {
+
+            current.style.display = 'grid';
+
+        }
+
+        languages.forEach(curr => {
+
+            if (curr === tag) {
+                current.style.display = 'grid';
+            }
+
+        })
+
+        tools.forEach(curr => {
+
+            if (curr === tag) {
+                current.style.display = 'grid';
+            }
+
+        })
+
+    })
 }
